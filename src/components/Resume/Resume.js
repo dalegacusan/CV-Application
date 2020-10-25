5b429e1f363e137b98083d62697072e0fe405884
Dale <dale@gacusan.com>
2020-10-25 14:30:04 +0800

    Resume.js displaying user information

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -44,7 +44,7 @@ export default function Resume({ state }) {
 
                 {/* Education */}
                 <div>
-                    <p style={{ textAlign: "center", fontWeight: "bold" }}>Education</p>
+                    <p style={{ textAlign: "center", fontWeight: "bold" }}>EDUCATION</p>
 
                     <hr />
 
@@ -82,89 +82,72 @@ export default function Resume({ state }) {
 
                 {/* Major Achievements */}
                 <div>
-                    <p style={{ textAlign: "center", fontWeight: "bold" }}>Major Achievements</p>
+                    <p style={{ textAlign: "center", fontWeight: "bold" }}>MAJOR ACHIEVEMENTS</p>
 
                     <hr />
 
-                    <div>
-                        <p style={{ fontWeight: "bold" }}>Yearbook Club</p>
-                        <p style={{ fontStyle: "italic" }}>Yearbook Design: Team Lead</p>
+                    {
+                        achievements.map(achievement => {
+                            const { companyName, position, contributions } = achievement;
 
-                        <ul style={{ listStyleType: "square" }}>
-                            <li>
-                                <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</span>
-                            </li>
-                            <li>
-                                <span>et quasi architecto beatae vitae dicta sunt explicabo</span>
-                            </li>
-                            <li>
-                                <span>dolores eos qui ratione voluptatem sequi nesciunt. Neque porro</span>
-                            </li>
-                        </ul>
-                    </div>
+                            return (
+                                <div>
+                                    <p style={{ fontWeight: "bold" }}>{companyName}</p>
+                                    <p style={{ fontStyle: "italic" }}>{position}</p>
 
-                    <div>
-                        <p style={{ fontWeight: "bold" }}>Santa Monica Newspaper</p>
-                        <p style={{ fontStyle: "italic" }}>Graphic Designer</p>
+                                    <ul style={{ listStyleType: "square" }}>
+                                        {
+                                            contributions.map(contribution => {
+                                                return (
+                                                    <li>
+                                                        <span>{contribution}</span>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
 
-                        <ul style={{ listStyleType: "square" }}>
-                            <li>
-                                <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem </span>
-                            </li>
-                            <li>
-                                <span>et quasi architecto beatae vitae dicta sunt explicabo</span>
-                            </li>
-                            <li>
-                                <span>dolores eos qui ratione voluptatem sequi nesciunt. Neque porro</span>
-                            </li>
-                            <li>
-                                <span>adipisci velit, sed quia non numquam eius</span>
-                            </li>
-                        </ul>
-                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
 
                 {/* Additional Skills */}
                 <div>
-                    <p style={{ textAlign: "center", fontWeight: "bold" }}>Additional Skills</p>
+                    <p style={{ textAlign: "center", fontWeight: "bold" }}>ADDITIONAL SKILLS</p>
 
                     <hr />
 
                     <ul style={{ listStyleType: "square" }}>
-                        <li>
-                            <span>Proficient in Photoshop and Adobe Creative Suite</span>
-                        </li>
-                        <li>
-                            <span>Basic knowledge of HTML</span>
-                        </li>
-                        <li>
-                            <span>Basic knowledge of Adobe Dreamweaver</span>
-                        </li>
-                        <li>
-                            <span>Keen eye for aesthetics with good understanding of image gradients</span>
-                        </li>
-                        <li>
-                            <span>Intermediate speaking level in spanish</span>
-                        </li>
+                        {
+                            skills.map(skill => {
+                                return (
+                                    <li>
+                                        <span>{skill}</span>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
 
                 {/* Awards, Honors, and Honorary Mentions */}
                 <div>
-                    <p style={{ textAlign: "center", fontWeight: "bold" }}>Awards, Honors, and Honorary Mentions</p>
+                    <p style={{ textAlign: "center", fontWeight: "bold" }}>AWARDS, HONORS, AND HONORARY MENTIONS</p>
 
                     <hr />
 
                     <ul style={{ listStyleType: "square" }}>
-                        <li>
-                            <span>Nominated for Macmillan Prize for Illustration in 2016</span>
-                        </li>
-                        <li>
-                            <span>Submitted artwork to Ocean Awareness Student Art Contest 2016</span>
-                        </li>
-                        <li>
-                            <span>Came in 2nd place in the Applied Arts 2016 Student Awards for submitting graphic design work under the category of Advertising</span>
-                        </li>
+                        {
+                            awards.map(award => {
+                                return (
+                                    <li>
+                                        <span>{award}</span>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
 
